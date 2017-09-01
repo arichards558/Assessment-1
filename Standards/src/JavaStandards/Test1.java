10e5f5ef17fd94ca1358006e6cc5af93df14bdde
arichards558 <31348479+arichards558@users.noreply.github.com>
2017-09-01 08:27:59 -0500

    Test thingy

---
+++ Standards/src/JavaStandards/Test1.java
@@ -0,0 +1,11 @@
+package JavaStandards;
+import javax.swing.JOptionPane;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Well, that escalated quickly.");
+
+	}
+
+}
---
+++ Standards/src/JavaStandards/Test3.java
@@ -0,0 +1,11 @@
+package JavaStandards;
+import javax.swing.JOptionPane;
+public class Test3 {
+
+	public static void main(String[] args) {
+		//System.out.println("I don't know what I'm doing...");
+		
+		JOptionPane.showMessageDialog(null, "I still don't know what I'm doing...");
+	}
+
+}
